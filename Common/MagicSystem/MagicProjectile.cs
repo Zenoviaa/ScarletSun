9d3096e10ad2424114584b20784a4352f22639aa
Scolon <scoloned@gmail.com>
2025-06-15 16:54:50 -0400

    Create MagicProjectile.cs

---
+++ Common/MagicSystem/MagicProjectile.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ModLoader;
+
+namespace ScarletSun.Common.MagicSystem
+{
+    internal class MagicProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.friendly = true;
+
+            //Alright, so we'll come back to this class later.
+
+        }
+
+        public override void AI()
+        {
+            base.AI();
+            //This is where we'll put all the custom AI
+        }
+    }
+}

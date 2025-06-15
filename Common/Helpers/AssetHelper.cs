d7e757053cae7ac4d958b2406f7592af61e86963
Scolon <scoloned@gmail.com>
2025-06-15 18:01:44 -0400

    create basic staff slot, add in textures

--- Common/Helpers/AssetHelper.cs
+++ Common/Helpers/AssetHelper.cs
@@ -1,8 +1,20 @@
-﻿namespace ScarletSun.Common.Helpers
+﻿using System;
+
+namespace ScarletSun.Common.Helpers
 {
     internal static class AssetHelper
     {
         public static string RootName => "ScarletSun";
         public static string EmptyTexture => RootName + "/Assets/Textures/Empty";
+        public static string DirectoryHere(this Type type)
+        {
+            string Texture = (type.Namespace).Replace('.', '/');
+            return Texture;
+        }
+        public static string PathHere(this Type type)
+        {
+            string Texture = (type.Namespace + "." + type.Name).Replace('.', '/');
+            return Texture;
+        }
     }
 }

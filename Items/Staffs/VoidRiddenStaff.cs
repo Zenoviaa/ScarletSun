a49224a5531def494eb14ce91ffa38eccc5e17a1
Scolon <scoloned@gmail.com>
2025-06-15 21:59:32 -0400

    added in void ridden staff

---
+++ Items/Staffs/VoidRiddenStaff.cs
@@ -0,0 +1,15 @@
+﻿using ScarletSun.Common.MagicSystem;
+using ScarletSun.Items.Elements;
+
+namespace ScarletSun.Items.Staffs
+{
+    internal class VoidRiddenStaff : Staff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            //For testing purposes
+            Element = new Radiant();
+        }
+    }
+}
